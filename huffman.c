1497df4202dd68564a1ab39896077c4cfddc0692
uday12381 <51029317+uday12381@users.noreply.github.com>
2020-05-28 09:17:34 +0530

    Add files via upload

---
+++ huffman.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+static int size=0;
+typedef struct node{
+	char ch;
+	int freq;
+	struct node* left;
+	struct node* right;
+}node;
+node* create(char ch,int freq){
+	node* temp=(node *)malloc(sizeof(node));
+	temp->ch=ch;
+	temp->freq=freq;
+	temp->left=temp->right=NULL;
+	return temp;
+}
+void swap(node** a,node** b){
+	node* temp=*a;
+	*a=*b;
+	*b=temp;
+}
+void display(node** arr,int n){
+	int i;
+	for(i=0;i<n;i++) printf("%c has %d frequency\n",arr[i]->ch,arr[i]->freq);
+	printf("\n");
+}
+void heapify(node** arr,int pos){
+	int smallest=pos;
+	int left=2*pos+1;
+	int right=2*pos+2;
+	if(left<size && arr[left]->freq < arr[smallest]->freq) smallest=left;
+	if(right<size && arr[right]->freq < arr[smallest]->freq) smallest=right;
+	if(smallest!=pos){
+		swap(&arr[pos],&arr[smallest]);
+		heapify(arr,smallest);
+	}
+}
+void heapify2(node** arr){
+	int i;
+	for(i=size/2-1;i>=0;i--) heapify(arr,i);
+}
+void insert(node** arr,node* element){
+	arr[size]=element;
+	size++;
+	heapify2(arr);
+	
+}
+node* extractMin(node** arr){
+	swap(&arr[0],&arr[size-1]);
+	size--;
+	heapify(arr,0);
+	return arr[size];
+}
+void display_util(int* arr,int n){
+	int i;
+	for(i=0;i<n;i++)
+		printf("%d ",arr[i]);
+	printf("\n");
+}
+void decode(node* ele,int* arr,int top){
+	if(ele->left){
+		arr[top]=0;
+		decode(ele->left,arr,top+1);
+	}
+	if(ele->right){
+		arr[top]=1;
+		decode(ele->right,arr,top+1);
+	}
+	if(ele->left==NULL){
+		printf("Character is %c decoded value is ",ele->ch);
+		display_util(arr,top);
+	}
+}
+void Huffman(node** arr){
+	heapify2(arr);
+	while(size>1){
+		node* first_min=extractMin(arr);
+		node* second_min=extractMin(arr);
+		node* temp=create('$',first_min->freq+second_min->freq);
+		temp->left=first_min;
+		temp->right=second_min;
+		insert(arr,temp);
+	}
+	int* arr1=(int* )malloc(sizeof(int)*100);
+	int top=0;
+	decode(arr[0],arr1,top);
+	free(arr1);
+}
+int main(){
+	printf("Enter the string \n");
+	char string[100];int i;int n=0;size=0;
+	scanf("%[^\n]",string);
+	int* freq=(int *)calloc(sizeof(int),26);
+	for(i=0;i<strlen(string);i++) if(string[i]!=' ') freq[string[i]-'a']++;
+	for(i=0;i<26;i++) if(freq[i]) size++;
+	node* array[size];n=size;int pos_util=0;
+	for(i=0;i<26;i++) if(freq[i]) array[pos_util++]=create((char)(i+'a'),freq[i]);
+	Huffman(array);
+	free(freq);
+	for(i=0;i<n;i++) free(array[i]);;
+	return 0;
+}
